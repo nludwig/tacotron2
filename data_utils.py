72c1b94f08bb5438c7d73167cbae4c9c822eab07
nludwig <nick.b.ludwig@gmail.com>
2021-01-16 17:02:10 -0800

    Initial changes to add linear FT features with magnitude & phase

--- data_utils.py
+++ data_utils.py
@@ -8,6 +8,63 @@ from utils import load_wav_to_torch, load_filepaths_and_text
 from text import text_to_sequence
 
 
+class TextLinFTLoader(torch.utils.data.Dataset):
+    """
+        1) loads audio,text pairs
+        2) normalizes text and converts them to sequences of one-hot vectors
+        3) computes linear FTs from audio files.
+    """
+    def __init__(self, audiopaths_and_text, hparams):
+        self.audiopaths_and_text = load_filepaths_and_text(audiopaths_and_text)
+        self.text_cleaners = hparams.text_cleaners
+        self.max_wav_value = hparams.max_wav_value
+        self.sampling_rate = hparams.sampling_rate
+        self.load_mel_from_disk = hparams.load_mel_from_disk
+        self.stft = layers.TacotronSTFT(
+            hparams.filter_length, hparams.hop_length, hparams.win_length,
+            hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
+            hparams.mel_fmax)
+        random.seed(hparams.seed)
+        random.shuffle(self.audiopaths_and_text)
+
+    def get_mel_text_pair(self, audiopath_and_text):
+        # separate filename and text
+        audiopath, text = audiopath_and_text[0], audiopath_and_text[1]
+        text = self.get_text(text)
+        mel = self.get_mel(audiopath)
+        return (text, mel)
+
+    def get_mel(self, filename):
+        if not self.load_mel_from_disk:
+            audio, sampling_rate = load_wav_to_torch(filename)
+            if sampling_rate != self.stft.sampling_rate:
+                raise ValueError("{} {} SR doesn't match target {} SR".format(
+                    sampling_rate, self.stft.sampling_rate))
+            audio_norm = audio / self.max_wav_value
+            audio_norm = audio_norm.unsqueeze(0)
+            audio_norm = torch.autograd.Variable(audio_norm, requires_grad=False)
+            magnitude, phase = self.stft.linear_ft(audio_norm)
+            linear_feature = torch.cat((magnitude, phase))
+            linear_feature = torch.squeeze(linear_feature, 0)
+        else:
+            linear_feature = torch.from_numpy(np.load(filename))
+            assert linear_feature.size(0) == self.stft.n_mel_channels, (
+                'Lin dimension mismatch: given {}, expected {}'.format(
+                    linear_feature.size(0), self.stft.n_mel_channels))
+
+        return linear_feature
+
+    def get_text(self, text):
+        text_norm = torch.IntTensor(text_to_sequence(text, self.text_cleaners))
+        return text_norm
+
+    def __getitem__(self, index):
+        return self.get_mel_text_pair(self.audiopaths_and_text[index])
+
+    def __len__(self):
+        return len(self.audiopaths_and_text)
+
+
 class TextMelLoader(torch.utils.data.Dataset):
     """
         1) loads audio,text pairs
--- layers.py
+++ layers.py
@@ -58,7 +58,6 @@ class TacotronSTFT(torch.nn.Module):
 
     def spectral_de_normalize(self, magnitudes):
         output = dynamic_range_decompression(magnitudes)
-        return output
 
     def mel_spectrogram(self, y):
         """Computes mel-spectrograms from a batch of waves
@@ -78,3 +77,21 @@ class TacotronSTFT(torch.nn.Module):
         mel_output = torch.matmul(self.mel_basis, magnitudes)
         mel_output = self.spectral_normalize(mel_output)
         return mel_output
+
+    def linear_ft(self, y):
+        """Computes linear FTs from a batch of waves
+        PARAMS
+        ------
+        y: Variable(torch.FloatTensor) with shape (B, T) in range [-1, 1]
+
+        RETURNS
+        -------
+        lin_ft_output: torch.FloatTensor of shape (B, n_mel_channels, T)
+        """
+        assert(torch.min(y.data) >= -1)
+        assert(torch.max(y.data) <= 1)
+
+        magnitudes, phases = self.stft_fn.transform(y)
+        magnitudes = magnitudes.data
+        phases = phases.data
+        return magnitudes, phases
